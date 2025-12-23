a4d0ecdd2c50aa3c084ed371f3431e62705612f3
Grigsan <109784198+Grigsan@users.noreply.github.com>
2025-12-23 11:58:54 +0700

    Говоруша: новый интерфейс, доски и логика

--- src/hooks/use-analytics.ts
+++ src/hooks/use-analytics.ts
@@ -34,3 +34,5 @@ export function useAnalytics() {
   return { stats, trackClick, getFrequentIds };
 }
 
+
+
--- src/hooks/use-custom-cards.ts
+++ src/hooks/use-custom-cards.ts
@@ -34,3 +34,5 @@ export function useCustomCards() {
   return { customCards, addCard, removeCard };
 }
 
+
+
